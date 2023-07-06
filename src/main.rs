f5aab107c08a5ebe3f88140b2efe4ee73e1ba29f
Jesse Jaggars <jjaggars@redhat.com>
2023-07-06 13:30:31 -0400

    extracting config a little

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use gethostname::gethostname;
 use influxdb::InfluxDbWriteable;
 use influxdb::{Client, Timestamp};
+use std::env;
 use std::time::{SystemTime, UNIX_EPOCH};
 use tokio::time::{self, Duration};
 
@@ -11,13 +12,18 @@ use nvml_wrapper::{
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let client =
-        Client::new("http://homeassistant:8086", "homeassistant").with_auth("user", "pass");
+    let username = env::var("INFLUXDB_USERNAME").expect("set INFLUXDB_USERNAME");
+    let password = env::var("INFLUXDB_PASSWORD").expect("set INFLUXDB_PASSWORD");
+    let hostname = env::var("INFLUXDB_HOSTNAME").unwrap_or("http://homeassistant:8086".to_string());
+    let database = env::var("INFLUXDB_DATABASE").unwrap_or("homeassistant".to_string());
+    let client = Client::new(hostname, database).with_auth(username, password);
 
     let nvml = Nvml::init()?;
     let device = nvml.device_by_index(0)?;
 
-    let hostname = gethostname();
+    let hn_os = gethostname();
+    let hostname = hn_os.to_str().unwrap_or("localhost");
+
     let mut interval = time::interval(Duration::from_secs(1));
 
     loop {
@@ -29,27 +35,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .into_query("pwr")
                 .add_field("value", device.power_usage()? / 1000)
                 .add_field("unit", "W")
-                .add_tag("hostname", hostname.to_str()),
+                .add_tag("hostname", hostname),
             Timestamp::Milliseconds(etime)
                 .into_query("gtemp")
                 .add_field("value", device.temperature(TemperatureSensor::Gpu)?)
                 .add_field("unit", "C")
-                .add_tag("hostname", hostname.to_str()),
+                .add_tag("hostname", hostname),
             Timestamp::Milliseconds(etime)
                 .into_query("mclk")
                 .add_field("value", device.clock_info(Clock::Memory)?)
                 .add_field("unit", "MHz")
-                .add_tag("hostname", hostname.to_str()),
+                .add_tag("hostname", hostname),
             Timestamp::Milliseconds(etime)
                 .into_query("pclk")
                 .add_field("value", device.clock_info(Clock::Graphics)?)
                 .add_field("unit", "MHz")
-                .add_tag("hostname", hostname.to_str()),
+                .add_tag("hostname", hostname),
             Timestamp::Milliseconds(etime)
                 .into_query("free")
                 .add_field("value", device.memory_info()?.free / (1024 * 1024))
                 .add_field("unit", "MHz")
-                .add_tag("hostname", hostname.to_str()),
+                .add_tag("hostname", hostname),
         ];
 
         client.query(points).await?;
